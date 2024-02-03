e92ab0eb1fca2910bbc3dc08d83191eefe592183
Gadmuthe <108920117+Gadmuthe@users.noreply.github.com>
2024-02-03 19:39:33 +0530

    Add files via upload

---
+++ while loops.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0, i = 1;
+
+    printf("Enter a positive integer N: ");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum of numbers from 1 to %d is %d\n", n, sum);
+
+    return 0;
+}
